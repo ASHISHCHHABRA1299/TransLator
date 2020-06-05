584fbd23663d227bcb1183a74cca00d0cd888272
hp <chhabraashish123@gmail.com>
2020-06-06 03:32:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/translator/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.translator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1,e2;
+    Button b;
+    boolean isdownload=false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.editText);
+        e2=(EditText)findViewById(R.id.editText2);
+        b=(Button)findViewById(R.id.button);
+        // Create an English-German translator:
+        FirebaseTranslatorOptions options =
+                new FirebaseTranslatorOptions.Builder()
+                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+                        .setTargetLanguage(FirebaseTranslateLanguage.HI)
+                        .build();
+        final FirebaseTranslator englishGermanTranslator =
+                FirebaseNaturalLanguage.getInstance().getTranslator(options);
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .requireWifi()
+                .build();
+
+        englishGermanTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void v) {
+                                // Model downloaded successfully. Okay to start translating.
+                                // (Set a flag, unhide the translation UI, etc.)
+                               isdownload=true;
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Model couldn’t be downloaded or other internal error.
+                                // ...
+                                isdownload=false;
+
+                            }
+                        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isdownload) {
+                    englishGermanTranslator.translate(e1.getText().toString())
+                            .addOnSuccessListener(
+                                    new OnSuccessListener<String>() {
+                                        @Override
+                                        public void onSuccess(@NonNull String translatedText) {
+                                            // Translation successful.
+                                            e2.setText(translatedText);
+                                        }
+                                    })
+                            .addOnFailureListener(
+                                    new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Error.
+                                            // ...
+                                        }
+                                    });
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Model is Not Downloaded", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
