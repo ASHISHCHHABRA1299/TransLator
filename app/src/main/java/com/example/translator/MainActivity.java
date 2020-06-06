5029bafb32f4775e1946a58e3dd14bb10ffaffa9
ASHISH CHHABRA <35839720+ASHISHCHHABRA1299@users.noreply.github.com>
2020-06-06 19:06:28 +0530

    MainActivity updated

--- app/src/main/java/com/example/translator/MainActivity.java
+++ app/src/main/java/com/example/translator/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.translator;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
 import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.languageid.FirebaseLanguageIdentification;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
@@ -22,18 +25,82 @@ public class MainActivity extends AppCompatActivity {
     EditText e1,e2;
     Button b;
     boolean isdownload=false;
+    public int code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e1=(EditText)findViewById(R.id.editText);
-        e2=(EditText)findViewById(R.id.editText2);
-        b=(Button)findViewById(R.id.button);
-        // Create an English-German translator:
+        e1 = (EditText) findViewById(R.id.editText);
+        e2 = (EditText) findViewById(R.id.editText2);
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseLanguageIdentification languageIdentifier =
+                        FirebaseNaturalLanguage.getInstance().getLanguageIdentification();
+                languageIdentifier.identifyLanguage(e1.getText().toString())
+                        .addOnSuccessListener(
+                                new OnSuccessListener<String>() {
+                                    @Override
+                                    public void onSuccess(@Nullable String languageCode) {
+                                        if (!languageCode.equals("und")) {
+                                             getlanguagecode(languageCode);
+                                        } else {
+                                            e2.setText("Can't Identify");
+                                        }
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Model couldn’t be loaded or other internal error.
+                                        // ...
+                                    }
+                                });
+
+            }
+        });
+
+
+
+    }
+    public void getlanguagecode(String c)
+    {
+        int code;
+        switch (c)
+        {
+            case "hi":
+            {
+                code=FirebaseTranslateLanguage.HI;
+                break;
+            }
+
+            case "ur":
+            {
+                code=FirebaseTranslateLanguage.UR;
+                break;
+            }
+
+            case "fr":
+            {
+                code=FirebaseTranslateLanguage.FR;
+                break;
+            }
+            default:
+                code =0;
+
+
+        }
+        translatetext(code);
+    }
+
+    private void translatetext(int code) {
+
         FirebaseTranslatorOptions options =
                 new FirebaseTranslatorOptions.Builder()
-                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
-                        .setTargetLanguage(FirebaseTranslateLanguage.HI)
+                        .setSourceLanguage(code)
+                        .setTargetLanguage(FirebaseTranslateLanguage.EN)
                         .build();
         final FirebaseTranslator englishGermanTranslator =
                 FirebaseNaturalLanguage.getInstance().getTranslator(options);
@@ -48,7 +115,25 @@ public class MainActivity extends AppCompatActivity {
                             public void onSuccess(Void v) {
                                 // Model downloaded successfully. Okay to start translating.
                                 // (Set a flag, unhide the translation UI, etc.)
-                               isdownload=true;
+
+                                englishGermanTranslator.translate(e1.getText().toString())
+                                        .addOnSuccessListener(
+                                                new OnSuccessListener<String>() {
+                                                    @Override
+                                                    public void onSuccess(@NonNull String translatedText) {
+                                                        // Translation successful.
+                                                        e2.setText(translatedText);
+                                                    }
+                                                })
+                                        .addOnFailureListener(
+                                                new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        // Error.
+                                                        // ...
+
+                                                    }
+                                                });
                             }
                         })
                 .addOnFailureListener(
@@ -57,37 +142,11 @@ public class MainActivity extends AppCompatActivity {
                             public void onFailure(@NonNull Exception e) {
                                 // Model couldn’t be downloaded or other internal error.
                                 // ...
-                                isdownload=false;
+                                Toast.makeText(MainActivity.this, "Model Not downloaded", Toast.LENGTH_SHORT).show();
+                            // model not get downloaded
 
                             }
                         });
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(isdownload) {
-                    englishGermanTranslator.translate(e1.getText().toString())
-                            .addOnSuccessListener(
-                                    new OnSuccessListener<String>() {
-                                        @Override
-                                        public void onSuccess(@NonNull String translatedText) {
-                                            // Translation successful.
-                                            e2.setText(translatedText);
-                                        }
-                                    })
-                            .addOnFailureListener(
-                                    new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            // Error.
-                                            // ...
-                                        }
-                                    });
-                }
-                else
-                {
-                    Toast.makeText(MainActivity.this, "Model is Not Downloaded", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
     }
 }
